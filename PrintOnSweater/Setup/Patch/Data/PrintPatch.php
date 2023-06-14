861e7ec994789a12295e208ca49e63d2b9446b13
Марков Дмитро <119345908+Dimon4ikk@users.noreply.github.com>
2023-06-14 13:10:37 +0300

    Add files via upload

---
+++ PrintOnSweater/Setup/Patch/Data/PrintPatch.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace DimasCorporation\PrintOnSweater\Setup\Patch\Data;
+
+use DimasCorporation\PrintOnSweater\Model\Attribute\Sourse\Position as Sourse;
+use Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\Patch\PatchVersionInterface;
+
+class PrintPatch implements DataPatchInterface, PatchVersionInterface
+{
+    private $moduleDataSetup;
+    private $eavSetupFactory;
+
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        EavSetupFactory          $eavSetupFactory
+    )
+    {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    public function apply()
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+        $eavSetup->addAttribute(
+            \Magento\Catalog\Model\Product::ENTITY,
+            'body_position',
+            [
+                'attribute_set' => 'Top',
+                'group' => 'General',
+                'type' => 'varchar',
+                'label' => 'Body Position',
+                'input' => 'select',
+                'source' => Sourse::class,
+                'frontend' => '',
+                'backend' => \Magento\Eav\Model\Entity\Attribute\Backend\ArrayBackend::class,
+                'required' => false,
+                'sort_order' => 50,
+                'global' => ScopedAttributeInterface::SCOPE_GLOBAL,
+                'is_used_in_grid' => false,
+                'is_visible_in_grid' => false,
+                'is_filterable_in_grid' => false,
+                'visible' => true,
+                'is_html_allowed_on_front' => true,
+                'visible_on_front' => true,
+                'used_in_product_listing' => true,
+            ]
+        );
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+
+    public function revert()
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+        $eavSetup->removeAttribute(
+            \Magento\Catalog\Model\Product::ENTITY,
+            'body_position');
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    public function getAliases()
+    {
+        return [];
+    }
+}
